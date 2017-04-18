2e16fa82e44a245a8cc81d175dccf4d6701f7560
wangwenshuai <1205483453@qq.com>
2017-04-19 07:17:54 +0800

    20170419

---
+++ jquerystudy/jquerystudy/ajax.php
@@ -0,0 +1,12 @@
+<?php
+// // echo $_POST['id'];
+// $arr=array(
+// 	array('id'=>'19','username'=>'wangwen'),
+// 	array('id'=>'20','username'=>'shuaige')
+// 	);
+// echo json_encode($arr);
+echo $_POST['id'];
+
+
+	
+?>
